9cea32ce3edc765d0128cb9da5b63a88d19fce9f
metacode <kjy901006@gmail.com>
2025-01-06 11:14:54 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+
+def main():
+    print("Starting CI Test...")
+    result = sum(range(1, 11))  # 1부터 10까지의 합
+    print(f"The sum of numbers from 1 to 10 is: {result}")
+
+if __name__ == "__main__":
+    main()
